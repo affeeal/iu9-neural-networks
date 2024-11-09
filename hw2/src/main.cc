3e37d04aab9c2871bdc065a996578a91969c8660
Ilya Afanasyev <affeeal@yandex.ru>
2024-11-09 17:37:32 +0300

    Finish hw2 report

--- hw2/src/main.cc
+++ hw2/src/main.cc
@@ -23,7 +23,7 @@ constexpr static auto kCfg = nn::Config{
     .monitor_testing_accuracy = true,
 };
 
-void Run3LeakyReluSoftmaxMSE() {
+void RunLeakyReluSoftmaxMSE() {
   const auto data_supplier =
       hw2::DataSupplier(kDefaultTrainPath, kDefaultTestPath, 0.0, 1.0);
   const auto training = data_supplier.GetTrainingData();
@@ -55,7 +55,7 @@ void Run3LeakyReluSoftmaxMSE() {
   matplot::show();
 }
 
-void Run3LeakyReluSoftmaxCrossEntropy() {
+void RunLeakyReluSoftmaxCrossEntropy() {
   const auto data_supplier =
       hw2::DataSupplier(kDefaultTrainPath, kDefaultTestPath, 0.0, 1.0);
   const auto training = data_supplier.GetTrainingData();
@@ -79,7 +79,8 @@ void Run3LeakyReluSoftmaxCrossEntropy() {
 
   const auto x = matplot::linspace(0, kCfg.epochs);
   matplot::plot(x, metrics.training_cost, x, metrics.testing_cost);
-  matplot::title("Leaky ReLU, Softmax + Cross-entropy training, testing cost");
+  matplot::title(
+      "Leaky ReLU, Softmax + Cross-entropy training, testing cost");
   matplot::show();
 
   matplot::plot(x, metrics.training_accuracy, x, metrics.testing_accuracy);
@@ -88,7 +89,7 @@ void Run3LeakyReluSoftmaxCrossEntropy() {
   matplot::show();
 }
 
-void Run3LeakyReluSoftmaxKlDivergence() {
+void RunLeakyReluSoftmaxKlDivergence() {
   const auto data_supplier =
       hw2::DataSupplier(kDefaultTrainPath, kDefaultTestPath, 10e-6, 1.0);
   const auto training = data_supplier.GetTrainingData();
@@ -125,37 +126,7 @@ void Run3LeakyReluSoftmaxKlDivergence() {
 }  // namespace
 
 int main(int argc, char *argv[]) {
-  Run3LeakyReluSoftmaxMSE();
-  Run3LeakyReluSoftmaxCrossEntropy();
-  Run3LeakyReluSoftmaxKlDivergence();
+  RunLeakyReluSoftmaxMSE();
+  RunLeakyReluSoftmaxCrossEntropy();
+  RunLeakyReluSoftmaxKlDivergence();
 }
-
-/*
- * 1x20
- * Epoch 199; training cost: 0.166286; training accuracy: 47518/50000; testing
- * cost: 0.193274; testing accuracy: 9421/10000; Epoch 199; training cost:
- * 0.192402; training accuracy: 47194/50000; testing cost: 0.217352; testing
- * accuracy: 9345/10000; Epoch 199; training cost: 0.17601; training accuracy:
- * 47346/50000; testing cost: 0.201559; testing accuracy: 9415/10000;
- *
- * 1x40
- * Epoch 199; training cost: 0.123245; training accuracy: 48121/50000; testing
- * cost: 0.161724; testing accuracy: 9527/10000; Epoch 199; training cost:
- * 0.131022; training accuracy: 48097/50000; testing cost: 0.173189; testing
- * accuracy: 9483/10000; Epoch 199; training cost: 0.118821; training accuracy:
- * 48233/50000; testing cost: 0.168346; testing accuracy: 9506/10000;
- *
- * 3x20
- * Epoch 199; training cost: 0.163502; training accuracy: 47504/50000; testing
- * cost: 0.206251; testing accuracy: 9416/10000; Epoch 199; training cost:
- * 0.157613; training accuracy: 47681/50000; testing cost: 0.216353; testing
- * accuracy: 9386/10000; Epoch 199; training cost: 0.163792; training accuracy:
- * 47487/50000; testing cost: 0.213412; testing accuracy: 9369/10000;
- *
- * 3x40
- * Epoch 199; training cost: 0.105351; training accuracy: 48422/50000; testing
- * cost: 0.190718; testing accuracy: 9481/10000; Epoch 199; training cost:
- * 0.118457; training accuracy: 48232/50000; testing cost: 0.17582; testing
- * accuracy: 9502/10000; Epoch 199; training cost: 0.111865; training accuracy:
- * 48315/50000; testing cost: 0.197848; testing accuracy: 9457/10000;
- */
