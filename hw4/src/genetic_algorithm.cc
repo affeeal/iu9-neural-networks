d47e377d87c67453fa1ded9222364b75ccfd9f8e
Ilya Afanasyev <affeeal@yandex.ru>
2024-12-17 00:39:06 +0300

    hw4: crossover chromosomes

--- hw4/src/genetic_algorithm.cc
+++ hw4/src/genetic_algorithm.cc
@@ -2,6 +2,7 @@
 
 #include <spdlog/spdlog.h>
 
+#include <algorithm>
 #include <cassert>
 #include <map>
 #include <numeric>
@@ -26,69 +27,98 @@ double Segment::get_right() const { return right_; }
 GeneticAlgorithm::GeneticAlgorithm(
     std::unique_ptr<IFitnessFunction>&& fitness_function,
     const ChromosomeSubclass subclass, const std::vector<Segment>& segments,
-    const std::size_t populations_number, const std::size_t population_size)
-    : random_engine_(std::random_device{}()),
-      fitness_function_(std::move(fitness_function)),
+    const GeneticAlgorithm::Configuration& cfg)
+    : fitness_function_(std::move(fitness_function)),
       subclass_(subclass),
-      populations_number_(populations_number),
-      population_size_(population_size) {
-  const auto genes_number = segments.size();
-  distributions_.reserve(genes_number);
+      cfg_(cfg),
+      genes_number_(segments.size()) {
+  genes_distributions_.reserve(genes_number_);
   for (auto&& segment : segments) {
-    distributions_.push_back(std::uniform_real_distribution<>(
+    genes_distributions_.push_back(std::uniform_real_distribution<>(
         segment.get_left(), segment.get_right()));
   }
 
-  population_.reserve(population_size);
-  for (std::size_t i = 0; i < population_size; ++i) {
+  population_.reserve(cfg.population_size);
+  for (std::size_t i = 0; i < cfg.population_size; ++i) {
     auto genes = std::vector<double>{};
-    genes.reserve(genes_number);
-    for (auto&& distribution : distributions_) {
-      genes.push_back(distribution(random_engine_));
+    genes.reserve(genes_number_);
+    for (auto&& distribution : genes_distributions_) {
+      genes.push_back(distribution(engine_));
     }
     population_.push_back(IChromosome::Create(std::move(genes), subclass_));
   }
 }
 
 std::shared_ptr<IChromosome> GeneticAlgorithm::Run() {
-  for (std::size_t i = 1; i <= populations_number_; ++i) {
-    const auto new_population = RouletteWheelSelection();
-    break;
-    // TODO
+  for (std::size_t i = 1; i <= cfg_.populations_number; ++i) {
+    auto chromosomes = RouletteWheelSelection();
+    Crossover(chromosomes);
+    std::shuffle(chromosomes.begin(), chromosomes.end(), engine_);
   }
 
   return nullptr;  // TODO
 }
 
 std::vector<std::shared_ptr<IChromosome>>
-GeneticAlgorithm::RouletteWheelSelection() {
+GeneticAlgorithm::RouletteWheelSelection() const {
   auto fitness_values = std::vector<double>{};
-  fitness_values.reserve(population_size_);
+  fitness_values.reserve(cfg_.population_size);
   for (auto&& chromosome : population_) {
     fitness_values.push_back(fitness_function_->Assess(*chromosome));
   }
 
-  auto partial_sum = std::vector<double>(population_size_);
+  auto partial_sum = std::vector<double>(cfg_.population_size);
   std::partial_sum(fitness_values.cbegin(), fitness_values.cend(),
                    partial_sum.begin());
 
   auto partial_sum_to_chromosome =
       std::map<double, std::shared_ptr<IChromosome>>{};
-  for (std::size_t i = 0; i < population_size_; ++i) {
+  for (std::size_t i = 0; i < cfg_.population_size; ++i) {
     partial_sum_to_chromosome.insert({partial_sum[i], population_[i]});
   }
 
-  auto new_population = std::vector<std::shared_ptr<IChromosome>>{};
-  new_population.reserve(population_size_);
+  auto selected_chromosomes = std::vector<std::shared_ptr<IChromosome>>{};
+  selected_chromosomes.reserve(cfg_.population_size);
   auto distribution = std::uniform_real_distribution<>{0, partial_sum.back()};
-  for (std::size_t i = 0; i < population_size_; ++i) {
-    const auto value = distribution(random_engine_);
+  for (std::size_t i = 0; i < cfg_.population_size; ++i) {
+    const auto value = distribution(engine_);
     const auto it = partial_sum_to_chromosome.upper_bound(value);
     assert(it != partial_sum_to_chromosome.end());
-    new_population.push_back(it->second);
+    selected_chromosomes.push_back(it->second);
   }
 
-  return new_population;
+  return selected_chromosomes;
+}
+
+void GeneticAlgorithm::Crossover(
+    std::vector<std::shared_ptr<IChromosome>>& population) const {
+  const auto parents_number = static_cast<std::size_t>(
+      cfg_.crossover_probability * cfg_.population_size);
+  static auto distribution = std::uniform_real_distribution<>{0.0, 1.0};
+  for (std::size_t i = 0; i + 1 < parents_number; i += 2) {
+    const auto alpha = distribution(engine_);
+
+    const auto& parent1_genes = population[i]->get_genes();
+    const auto& parent2_genes = population[i + 1]->get_genes();
+
+    auto offspring1_genes = std::vector<double>{};
+    offspring1_genes.reserve(genes_number_);
+    for (std::size_t j = 0; j < genes_number_; ++j) {
+      offspring1_genes.push_back(alpha * parent1_genes[j] +
+                                 (1 - alpha) * parent2_genes[j]);
+    }
+
+    auto offspring2_genes = std::vector<double>{};
+    offspring2_genes.reserve(genes_number_);
+    for (std::size_t j = 0; j < genes_number_; ++j) {
+      offspring2_genes.push_back((1 - alpha) * parent1_genes[j] +
+                                 alpha * parent2_genes[j]);
+    }
+
+    population[i] = IChromosome::Create(std::move(offspring1_genes), subclass_);
+    population[i + 1] =
+        IChromosome::Create(std::move(offspring2_genes), subclass_);
+  }
 }
 
 }  // namespace nn
--- hw4/src/genetic_algorithm.h
+++ hw4/src/genetic_algorithm.h
@@ -20,25 +20,37 @@ class Segment final {
 };
 
 class GeneticAlgorithm final {
-  std::mt19937 random_engine_;
+ public:
+  // TODO: Validate the configuration
+  struct Configuration final {
+    std::size_t populations_number;
+    std::size_t population_size;
+    double crossover_probability;
+    double mutation_probability;
+  };
+
+ private:
+  std::mt19937 engine_{std::random_device{}()};
+  std::uniform_real_distribution<double> zero_one_distribution_{0.0, 1.0};
+
   std::unique_ptr<IFitnessFunction> fitness_function_;
   ChromosomeSubclass subclass_;
-  std::size_t populations_number_;
-  std::size_t population_size_;
-  std::vector<std::uniform_real_distribution<double>> distributions_;
+  std::vector<std::uniform_real_distribution<double>> genes_distributions_;
   std::vector<std::shared_ptr<IChromosome>> population_;
+  Configuration cfg_;
+  std::size_t genes_number_;
 
  public:
   GeneticAlgorithm(std::unique_ptr<IFitnessFunction>&& fitness_function,
                    const ChromosomeSubclass subclass,
                    const std::vector<Segment>& segments,
-                   const std::size_t populations_number,
-                   const std::size_t population_size);
+                   const Configuration& cfg);
 
   std::shared_ptr<IChromosome> Run();
 
  private:
-  std::vector<std::shared_ptr<IChromosome>> RouletteWheelSelection();
+  std::vector<std::shared_ptr<IChromosome>> RouletteWheelSelection() const;
+  void Crossover(std::vector<std::shared_ptr<IChromosome>>& population) const;
 };
 
 }  // namespace nn
