bab16e009467b7a92cacf2dba766b716a456f084
Ilya Afanasyev <affeeal@yandex.ru>
2024-12-26 07:06:40 +0300

    feat hw5: add vgg16

--- hw5/main.py
+++ hw5/main.py
@@ -6,7 +6,10 @@ import torch.nn.functional as functional
 
 from torchvision import datasets, transforms
 
-DATA_PATH = 'data/'
+DATA_PATH = '../datasets/'
+BATCH_SIZE = 100
+MOMENTUM = 0.9
+EPOCHS = 20
 
 
 class LeNet(nn.Module):
@@ -28,7 +31,41 @@ class LeNet(nn.Module):
         return out
 
 
-def training_loop(n_epochs, optimizer, model, loss_fn, train_loader):
+class VGG16(nn.Module):
+    def __init__(self, dropout_p):
+        super().__init__()
+        self.conv11 = nn.Conv2d(3, 128, kernel_size=3, padding=1)
+        self.conv12 = nn.Conv2d(128, 128, kernel_size=3, padding=1)
+        self.conv21 = nn.Conv2d(128, 256, kernel_size=3, padding=1)
+        self.conv22 = nn.Conv2d(256, 256, kernel_size=3, padding=1)
+        self.conv31 = nn.Conv2d(256, 512, kernel_size=3, padding=1)
+        self.conv32 = nn.Conv2d(512, 512, kernel_size=3, padding=1)
+        self.fc1 = nn.Linear(512 * 4 * 4, 1024)
+        self.fc1_dropout = nn.Dropout(p=dropout_p)
+        self.fc2 = nn.Linear(1024, 1024)
+        self.fc2_dropout = nn.Dropout(p=dropout_p)
+        self.fc3 = nn.Linear(1024, 10)
+
+    def forward(self, x):
+        out = torch.relu(self.conv11(x))     # 3 x 32 x 32   -> 128 x 32 x 32
+        out = torch.relu(self.conv12(out))   # 128 x 32 x 32 -> 128 x32 x 32
+        out = functional.max_pool2d(out, 2)  # 128 x 32 x 32 -> 128 x 16 x 16
+        out = torch.relu(self.conv21(out))   # 128 x 16 x 16 -> 256 x 16 x 16
+        out = torch.relu(self.conv22(out))   # 256 x 16 x 16 -> 256 x 16 x 16
+        out = functional.max_pool2d(out, 2)  # 256 x 16 x 16 -> 256 x 8 x 8
+        out = torch.relu(self.conv31(out))   # 256 x 8 x 8   -> 512 x 8 x 8
+        out = torch.relu(self.conv32(out))   # 512 x 8 x 8   -> 512 x 8 x 8
+        out = functional.max_pool2d(out, 2)  # 512 x 8 x 8   -> 512 x 4 x 4
+        out = out.view(-1, 512 * 4 * 4)
+        out = torch.relu(self.fc1(out))      # 512 x 4 x 4 -> 1024
+        out = self.fc1_dropout(out)
+        out = torch.relu(self.fc2(out))      # 1024 -> 1024
+        out = self.fc2_dropout(out)
+        out = self.fc3(out)                  # 1024 -> 10
+        return out
+
+
+def train(n_epochs, optimizer, model, loss_fn, train_loader):
     for epoch in range(1, n_epochs + 1):
         loss_train = 0.0
         for imgs, labels in train_loader:
@@ -48,7 +85,7 @@ def training_loop(n_epochs, optimizer, model, loss_fn, train_loader):
             loss_train / len(train_loader)))
 
 
-def accuracy(model, train_loader, test_loader):
+def calculate_accuracy(model, train_loader, test_loader):
     accdict = {}
     for name, loader in [("train", train_loader), ("test", test_loader)]:
         correct = 0
@@ -68,30 +105,82 @@ def accuracy(model, train_loader, test_loader):
     return accdict
 
 
-if __name__ == '__main__':
-    device = (torch.device('cuda') if torch.cuda.is_available()
-              else torch.device('cpu'))
-    print(f"Training on device {device}.")
+device = (torch.device('cuda') if torch.cuda.is_available()
+          else torch.device('cpu'))
+loss_fn = nn.CrossEntropyLoss()
 
-    mnist_train = datasets.MNIST(
-        DATA_PATH, train=True, download=True, transform=transforms.ToTensor())
-    mnist_test = datasets.MNIST(
-        DATA_PATH, train=False, download=True, transform=transforms.ToTensor())
+mnist_train = datasets.MNIST(
+    DATA_PATH, train=True, download=True, transform=transforms.ToTensor())
+mnist_test = datasets.MNIST(
+    DATA_PATH, train=False, download=True, transform=transforms.ToTensor())
 
-    train_loader = torch.utils.data.DataLoader(
-        mnist_train, batch_size=100, shuffle=True)
-    test_loader = torch.utils.data.DataLoader(
-        mnist_test, batch_size=100, shuffle=True)
+cifar10_train = datasets.CIFAR10(
+    DATA_PATH, train=True, download=True, transform=transforms.ToTensor())
+cifar10_test = datasets.CIFAR10(
+    DATA_PATH, train=False, download=True, transform=transforms.ToTensor())
 
-    model = LeNet().to(device=device)
-    loss_fn = nn.CrossEntropyLoss()
 
-    for name, optimizer in [
-        ('SGD', optim.SGD(model.parameters(), lr=1e-2)),
-        ('Adadelta', optim.Adadelta(model.parameters(), lr=1e-2)),
-        ('NAG', optim.SGD(model.parameters(), lr=1e-2, momentum=0.9, nesterov=True)),
-            ('Adam', optim.Adam(model.parameters(), lr=1e-3))]:
-        print(f'Optimization {name}.')
-        training_loop(n_epochs=20, optimizer=optimizer, model=model,
-                      loss_fn=loss_fn, train_loader=train_loader)
-        accuracy(model, train_loader, test_loader)
+# LeNet, MNIST
+model = LeNet().to(device=device)
+train_loader = torch.utils.data.DataLoader(
+    mnist_train, batch_size=BATCH_SIZE, shuffle=True)
+test_loader = torch.utils.data.DataLoader(
+    mnist_test, batch_size=BATCH_SIZE, shuffle=True)
+
+# SGD
+optimizer = optim.SGD(model.parameters(), lr=1e-2)
+train(n_epochs=EPOCHS, optimizer=optimizer, model=model,
+      loss_fn=loss_fn, train_loader=train_loader)
+calculate_accuracy(model, train_loader, test_loader)
+
+# Adadelta
+optimizer = optim.Adadelta(model.parameters(), lr=1e-2)
+train(n_epochs=EPOCHS, optimizer=optimizer, model=model,
+      loss_fn=loss_fn, train_loader=train_loader)
+calculate_accuracy(model, train_loader, test_loader)
+
+# NAG
+optimizer = optim.SGD(model.parameters(), lr=1e-2,
+                      momentum=MOMENTUM, nesterov=True)
+train(n_epochs=EPOCHS, optimizer=optimizer, model=model,
+      loss_fn=loss_fn, train_loader=train_loader)
+calculate_accuracy(model, train_loader, test_loader)
+
+# Adam
+optimizer = optim.Adam(model.parameters(), lr=1e-3)
+train(n_epochs=EPOCHS, optimizer=optimizer, model=model,
+      loss_fn=loss_fn, train_loader=train_loader)
+calculate_accuracy(model, train_loader, test_loader)
+
+
+# VGG16, CIFAR10
+model = VGG16(dropout_p=0.4).to(device=device)
+train_loader = torch.utils.data.DataLoader(
+    cifar10_train, batch_size=BATCH_SIZE, shuffle=True)
+test_loader = torch.utils.data.DataLoader(
+    cifar10_test, batch_size=BATCH_SIZE, shuffle=True)
+
+# SGD
+optimizer = optim.SGD(model.parameters(), lr=1e-1)
+train(n_epochs=EPOCHS, optimizer=optimizer, model=model,
+      loss_fn=loss_fn, train_loader=train_loader)
+calculate_accuracy(model, train_loader, test_loader)
+
+# Adadelta
+optimizer = optim.Adadelta(model.parameters(), lr=1e-1)
+train(n_epochs=EPOCHS, optimizer=optimizer, model=model,
+      loss_fn=loss_fn, train_loader=train_loader)
+calculate_accuracy(model, train_loader, test_loader)
+
+# NAG
+optimizer = optim.SGD(model.parameters(), lr=1e-1,
+                      momentum=MOMENTUM, nesterov=True)
+train(n_epochs=EPOCHS, optimizer=optimizer, model=model,
+      loss_fn=loss_fn, train_loader=train_loader)
+calculate_accuracy(model, train_loader, test_loader)
+
+# Adam
+optimizer = optim.Adam(model.parameters(), lr=1e-1)
+train(n_epochs=EPOCHS, optimizer=optimizer, model=model,
+      loss_fn=loss_fn, train_loader=train_loader)
+calculate_accuracy(model, train_loader, test_loader)
